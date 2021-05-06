3ab7531e75499a74b2221746a02b08ba40e76f29
yihu-lv <1720427782@qq.com>
2021-05-06 19:00:01 +0800

    模块化

---
+++ src/views/money/components/Notes.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <label class="note">
+      <span>备注</span>
+      <input type="text" placeholder="这里添加备注" />
+    </label>
+  </div>
+</template>
+<script lang='ts'>
+export default {
+name:''
+}
+</script>
+<style lang='scss' scoped>
+.note {
+  display: flex;
+  align-items: center;
+  font-size: 14px;
+  background: #f5f5f5;
+  padding-left: 16px;
+  span {
+    padding-right: 16px;
+  }
+  input {
+    flex-grow: 1;
+    height: 64px;
+    border: none;
+    background: transparent;
+    padding-right: 16px;
+  }
+}
+</style>
---
+++ src/views/money/components/Tags.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="tags">
+    <div class="tag-add">
+      <button>新增标签</button>
+    </div>
+    <ul class="tags-list">
+      <li>衣</li>
+      <li>食</li>
+      <li>住</li>
+      <li>行</li>
+    </ul>
+  </div>
+</template>
+<script lang='ts'>
+export default {
+name:'Tags'
+}
+</script>
+<style lang='scss' scoped>
+.tags {
+  font-size: 14px;
+  padding: 16px;
+  display: flex;
+  flex-direction: column-reverse;
+  > .tags-list {
+    display: flex;
+    flex-wrap: wrap;
+    > li {
+      background: #d9d9d9;
+      $h: 24px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 12px;
+      margin-right: 12px;
+      margin-top: 8px;
+    }
+  }
+  > .tag-add {
+    padding-top: 16px;
+    button {
+      background: transparent;
+      color: #d9d9d9;
+      border: none;
+      border-bottom: 1px solid;
+      padding: 0 4px;
+    }
+  }
+}
+</style>
